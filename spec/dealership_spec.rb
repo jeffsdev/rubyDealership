ca363d95fc562bab8229841353bd6aa08c6e6d49
Jeff Seymour and Riley Starnes <seym1585@gmail.com>
2016-01-13 10:47:44 -0800

    add initial spec and code for Dealership class

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -1,7 +1,9 @@
 require('rspec')
 require('vehicle')
-# require('dealership')
-
+require('dealership')
+#
+# Specs for Vehicle class ###############################
+#
 describe('Vehicle') do
   describe('#make') do
     it('returns the make of the vehicle') do
@@ -30,42 +32,60 @@ describe('Vehicle') do
     end
   end
 
-describe('#save') do
-  it('adds a vehicle to the array of saved vehicles') do
-    test_vehicle = Vehicle.new('Ford', 'Ranger', 2003)
-    test_vehicle.save()
-    expect(Vehicle.all()).to(eq([test_vehicle]))
+  describe('#save') do
+    it('adds a vehicle to the array of saved vehicles') do
+      test_vehicle = Vehicle.new('Ford', 'Ranger', 2003)
+      test_vehicle.save()
+      expect(Vehicle.all()).to(eq([test_vehicle]))
+    end
+  end
+
+  describe('.clear') do
+    it('empties out all of the saved vehicles') do
+      Vehicle.new('Ford', 'Ranger', 2003).save()
+      Vehicle.clear()
+      expect(Vehicle.clear()).to(eq([]))
+    end
+  end
+
+  describe('#age') do
+    it('return the vehicles age') do
+      test_vehicle = Vehicle.new('Ford', 'Ranger', 2003)
+      expect(test_vehicle.age()).to(eq(13))
+    end
+  end
+
+  describe('#worth_buying?') do
+    it('returns true if the car is American made and less than 15 years old') do
+      test_vehicle = Vehicle.new('Ford', 'Ranger', 2003)
+      expect(test_vehicle.worth_buying?()).to(eq(true))
+    end
+  end
+
+  describe('#id') do
+    it('returns the id of the vehicle') do
+      test_vehicle = Vehicle.new('Ford', 'Ranger', 2003)
+      test_vehicle.save()
+      expect(test_vehicle.id()).to(eq(1))
+    end
   end
 end
 
-describe('.clear') do
-  it('empties out all of the saved vehicles') do
-    Vehicle.new('Ford', 'Ranger', 2003).save()
-    Vehicle.clear()
-    expect(Vehicle.clear()).to(eq([]))
-  end
-end
+#
+# Specs for Dealership class ###############################
+#
 
-describe('#age') do
-  it('return the vehicles age') do
-    test_vehicle = Vehicle.new('Ford', 'Ranger', 2003)
-    expect(test_vehicle.age()).to(eq(13))
+describe('Dealership') do
+  describe('#name') do
+    it('returns the name of the dealership') do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      expect(test_dealership.name()).to(eq("Bob's Used Cars"))
+    end
   end
-end
 
-describe('#worth_buying?') do
-  it('returns true if the car is American made and less than 15 years old') do
-    test_vehicle = Vehicle.new('Ford', 'Ranger', 2003)
-    expect(test_vehicle.worth_buying?()).to(eq(true))
-  end
-end
 
-describe('#id') do
-  it('returns the id of the vehicle') do
-    test_vehicle = Vehicle.new('Ford', 'Ranger', 2003)
-    test_vehicle.save()
-    expect(test_vehicle.id()).to(eq(1))
-  end
-end
+
+
+
 
 end
